cc4de321e5da525484cdf6def60ab0fbfbc1a959
Unknown <ttmitm012@gmail.com>
2017-11-03 14:54:12 +0100

    Changing !help and adding a music queue

--- main.py
+++ main.py
@@ -11,6 +11,11 @@ token = config.get('credentials', 'TOKEN')
 
 bot = discord.Client()
 
+audioQueue = []
+
+players = {}
+
+
 @bot.event #startup
 async def on_ready():
     print('Bot running')
@@ -27,9 +32,15 @@ async def on_message(message):
 #help command
     if message.content.lower().startswith('!help'):
         helpCommand = ('**__Commands__**\n\n'
-                       '___Image(Imgur) commands___:\n'
+                       '__Image(Imgur) commands__:\n'
                        '!top  --> Shows the top viewed image on the imgur frontpage\n'
-                       '!img [tag]  --> Shows a random image with that tag')
+                       '!img [tag]  --> Shows a random image with that tag\n\n'
+                       '__Music commands__\n!play [yt link]  --> Bot joins the voice channel and plays a song\n'
+                       '!add [yt link]  --> Queues a song\n'
+                       '!skip  --> Skips to the next song in the queue\n'
+                       '!pause  --> Pauses the current song\n'
+                       '!resume  --> Continues the current song\n'
+                       '!quit  --> Removes the bot from the voice channel')
         await bot.send_message(message.channel, helpCommand)
 
 # ----------------------- IMAGE COMMANDS ------------------------------
@@ -57,6 +68,7 @@ async def on_message(message):
 
 #Lets the bot join your voice channel and plays a song
     elif message.content.lower().startswith('!play'):
+        yt_url = message.content[6:]
         channel = message.author.voice.voice_channel
         if not channel:
             await bot.send_message(message.channel, 'You have to be in a voice channel')
@@ -64,21 +76,44 @@ async def on_message(message):
             voice_client = bot.voice_client_in(message.server)
             if not voice_client:
                 voice = await bot.join_voice_channel(channel)
-                yt_url = message.content[6:]
                 if not yt_url:
                     await bot.send_message(message.channel, 'You have to provide a link to a youtube video')
                 else:
                     player = await voice.create_ytdl_player(yt_url)
+                    players[message.server.id] = player
                     player.start()
             else:
-                await voice_client.disconnect()
-                voice = await bot.join_voice_channel(channel)
-                yt_url = message.content[6:]
-                if not yt_url:
-                    await bot.send_message(message.channel, 'You have to provide a link to a youtube video')
-                else:
-                    player = await voice.create_ytdl_player(yt_url)
-                    player.start()
+                await bot.send_message(message.channel, 'Please use !add [Youtube link] to add songs to the queue')
+
+#Pauses the song
+    elif message.content.lower().startswith('!pause'):
+        players[message.server.id].pause()
+
+#Continues the song
+    elif message.content.lower().startswith('!resume'):
+        players[message.server.id].resume()
+
+#Adds a song to the queue
+    elif message.content.lower().startswith('!add'):
+        yt_url = message.content[5:]
+        audioQueue.append(yt_url)
+        await bot.add_reaction(message, '✅')
+
+#Shows the queue
+    elif message.content.lower().startswith('!queue'):
+        await bot.send_message(message.channel, audioQueue)
+
+#Skips to the next song
+    elif message.content.lower().startswith('!skip'):
+        if not audioQueue:
+            await bot.send_message(message.channel, 'The queue is empty')
+        else:
+            voice = bot.voice_client_in(message.server)
+            players[message.server.id].stop()
+            player =await voice.create_ytdl_player(audioQueue[0])
+            players[message.server.id] = player
+            player.start()
+            del audioQueue[0]
 
 
 bot.run(token)
